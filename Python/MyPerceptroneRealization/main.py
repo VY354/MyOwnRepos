6af2fedbe9ad1a33e65963a2e93da94d2ed18863
rayxxx <vy354nhk@gmail.com>
2022-04-24 20:05:14 +0300

    made ANN class base

--- Python/MyPerceptroneRealization/main.py
+++ Python/MyPerceptroneRealization/main.py
@@ -1,56 +1,96 @@
 from math import *
 import numpy as np
 
+from dataclasses import dataclass, field, fields, asdict, astuple
+
+@dataclass
+class ANN:
+    inputNeuronsNumber: int = field(default=0, init=True)
+    outputNeuronsNumber: int = field(default=0, init=True)
+    hiddenLayersStructure: list = field(default_factory=list, init=True)
+    __fullLayersStructure: list = field(default_factory=list)
+
+    __biases: list = field(default_factory=list, init=False)
+    __weights: list = field(default_factory=list, init=False)
+    __layers: list = field(default_factory=list, init=False)
+
+    activationFunction: object = field(default=(lambda x: x), init=True)
+    useBiases: bool = field(default=False, init=True)
+
+    def initialize(self):
+        # creating full layers structure (include input and output)
+        self.__fullLayersStructure = [[1, self.inputNeuronsNumber]] + self.hiddenLayersStructure + [[1, self.outputNeuronsNumber]]
+
+        # generate in and out layers and 1d array
+        inputLayer = np.ones(self.inputNeuronsNumber)
+        outputLayer = np.ones(self.outputNeuronsNumber)
+
+        # generate layers array with corresponding layers structure (num of hidden layers and number of
+        # neurons in each hidden layer)
+        self.__layers = []
+        self.__layers.append(inputLayer)
+        for layerGroup in self.hiddenLayersStructure:
+            for l in range(layerGroup[0]):
+                self.__layers.append([np.ones(layerGroup[1])])
+        self.__layers.append(outputLayer)
+        self.__layers = [np.array(layer) for layer in self.__layers]
+
+        # create weights matrices depending on layers structure
+        self.__weights = []
+        for i in range(len(self.__fullLayersStructure) - 1):
+            self.__weights.append(np.ones((self.__fullLayersStructure[i + 1][1], self.__fullLayersStructure[i][1])))
+            for j in range(self.__fullLayersStructure[i + 1][0] - 1):
+                self.__weights.append(np.ones((self.__fullLayersStructure[i + 1][1], self.__fullLayersStructure[i + 1][1])))
+
+        # create biases list if needed
+        if self.useBiases:
+            self.__biases = [1 for i in range(np.sum([x[0] for x in self.hiddenLayersStructure]))]
+
+    # propagate input data
+    def run(self, inputData: list):
+        self.__layers[0] = inputData
+        for i in range(1, len(self.__layers)):
+            self.__layers[i] = np.array(
+                [self.activationFunction(np.dot(self.__layers[i - 1], w) +
+                                         (self.__biases[i - 1] if (self.__biases and i < len(self.__layers) - 1) else 0))
+                 for w in self.__weights[i - 1]])
+
+        for l in self.__layers:
+            print(l)
+
+    def __propagation(self):
+        pass
+
+    def __backPropagation(self):
+        pass
+
+    def learn(self):
+        pass
+
 
 def main():
+    # input layer neurons number
+    inn = 1
+    # output layer neurons number
+    onn = 1
+    # hidden layers structure: array of type [ [ln1 , nn1], [... , ...], ... ],
+    # where ln -> layers number, nn -> neurons number in this layers
+    hlstruct = [[4, 3]]
+
     actFunc = lambda x: x
 
-    inn = 1  # input layer neurons number
-    onn = 1  # output layer neurons number
+    ann = ANN(
+        inputNeuronsNumber=inn,
+        outputNeuronsNumber=onn,
+        hiddenLayersStructure=hlstruct,
+        useBiases=False,
+        activationFunction=actFunc
+    )
 
-    # hidden layers structure: array of type [ [ln1 , nn1], [... , ...], ... ],
-    # where ln -> layers number, nn -> neurpns number in this layers
-    hlstruct = [[5, 3]]
+    ann.initialize()
 
-    il = np.ones(inn)  # input layer
-    ol = np.ones(onn)  # output layer
-    hls = [np.ones((dim[0], dim[1])) for dim in hlstruct]  # hidden layers
-    biases = [1 for i in range(np.sum([x[0] for x in hlstruct]))]  # biases
-
-    fls = [[1, inn]] + hlstruct + [[1, onn]]  # full layers structure (include il and ol)
-
-    layers = []  # list of all layers, made corresponding to with full layers structure
-    layers.append(il)
-    for lg in hls:
-        for l in lg:
-            layers.append(l)
-    layers = [np.array(l) for l in layers]
-    layers.append(ol)
-
-    weights = []
-    for i in range(len(fls) - 1):
-        weights.append(np.ones((fls[i + 1][1], fls[i][1])))
-        for j in range(fls[i + 1][0] - 1):
-            weights.append(np.ones((fls[i + 1][1], fls[i + 1][1])))
-
-    propagate(np.ones(inn), layers, weights, biases, actFunc)
-
-
-def propagate(input, layers, weights, biases, actFunc):
-    layers[0] = input
-
-    # forward propagation
-    # for each layers from second:
-    # for current layer generate list of neurons values as:
-    # for each row in current weights matrix, get dot prod. of
-    # prev. layer neurons values with this row plus bias and pass it to activation function
-    # as a result -> get current layer neurons values
-    for i in range(1, len(layers)):
-        layers[i] = np.array(
-            [actFunc(np.dot(layers[i - 1], w) + (biases[i - 1] if i < len(layers) - 1 else 0)) for w in weights[i - 1]])
-
-    for l in layers:
-        print(l)
+    inputData = [1]
+    ann.run(inputData)
 
 
 if __name__ == '__main__':
