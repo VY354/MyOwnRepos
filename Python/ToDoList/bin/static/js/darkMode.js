80029018ad86855c82e1f4478c427b69f8f6496c
rayxxx <vy354nhk@gmail.com>
2022-07-22 10:54:30 +0300

    FastAPI to-do list web app

--- Python/ToDoList/bin/static/js/darkMode.js
+++ Python/ToDoList/bin/static/js/darkMode.js
@@ -1,14 +1,3 @@
-
-// document
-//     .getElementsByClassName("add-task")[0]
-//     .addEventListener("click", (e) => {
-//         saveCookie();
-//     });
-
-// if (document.cookie) {
-//     toggleDarkMode();
-// }
-
 function toggleDarkMode() {
     saveCookie();
 
@@ -34,32 +23,7 @@ function toggleDarkMode() {
         el.classList.toggle("dark");
     });
 
-    document
-        .getElementsByClassName("dark-mode-btn")[0]
-        .classList.toggle("dark");
+    document.getElementsByClassName("dark-mode-btn")[0].classList.toggle("dark");
 
     document.getElementsByClassName("add-task-btn")[0].classList.toggle("dark");
-}
-
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-
-    console.log("deleted cookie: " + document.cookie);
-}
-
-function saveCookie() {
-    if (document.cookie) {
-        deleteAllCookies();
-    } else {
-        document.cookie = "dark-mode=true";
-
-        console.log("new cookie: " + document.cookie);
-    }
 }
